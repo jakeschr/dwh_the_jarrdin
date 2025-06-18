12787ba8392007e3fe3da829e2f2afc13a29c245
deboschr <debospencher@gmail.com>
2025-06-18 23:39:04 +0700

    asa

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userRoute = require("./user.route");
-// const databaseRoute = require("./database.route");
+const databaseRoute = require("./database.route");
 // const pipelineRoute = require("./pipeline.route");
 // const jobRoute = require("./job.route");
 // const logRoute = require("./log.route");
@@ -11,10 +11,10 @@ router.get("/", (req, res) => {
 	res.status(200).json("Wellcome to Website Content Generator APIs");
 });
 
-router.use("/", userRoute);
-// router.use("/", databaseRoute);
-// router.use("/", pipelineRoute);
-// router.use("/", jobRoute);
-// router.use("/", logRoute);
+router.use("/api", userRoute);
+router.use("/api", databaseRoute);
+// router.use("/api", pipelineRoute);
+// router.use("/api", jobRoute);
+// router.use("/api", logRoute);
 
 module.exports = { router };
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,13 +4,13 @@ const { UserController } = require("../controllers/user.controller");
 
 const router = express.Router();
 
-router.post("/api/:version/auth/signin", UserController.signin);
-router.delete("/api/:version/auth/signout", UserController.signout);
-router.post("/api/:version/auth/signup", authorization, UserController.signup);
+router.post("/:version/auth/signin", UserController.signin);
+router.delete("/:version/auth/signout", UserController.signout);
+router.post("/:version/auth/signup", authorization, UserController.signup);
 
-router.get("/api/:version/user", authorization, UserController.findSummary);
-router.get("/api/:version/user/:id", authorization, UserController.findDetail);
-router.patch("/api/:version/user", authorization, UserController.update);
-router.delete("/api/:version/user/:id", authorization, UserController.delete);
+router.get("/:version/user", authorization, UserController.findSummary);
+router.get("/:version/user/:id", authorization, UserController.findDetail);
+router.patch("/:version/user", authorization, UserController.update);
+router.delete("/:version/user/:id", authorization, UserController.delete);
 
 module.exports = router;
